8417d486c15dafeb348693c6014c58645759db23
yee2542 <yee2542gtl@gmail.com>
2019-11-11 23:38:04 +0700

    bug fix

--- sa_single_objective.py
+++ sa_single_objective.py
@@ -1,6 +1,6 @@
 from itertools import permutations
 from datastruct import readfile, parse_csv, parse_matrix_dist, Place
-from random import shuffle
+from random import shuffle, seed, randrange, random
 
 node = readfile('place.csv')
 node = parse_csv(node)
@@ -31,7 +31,6 @@ perm = [1, 2, 3, 4, 5, 6, 7, 8]
 #     print(i)
 
 def fitness(d = []):
-    # node.node[]
     maxLength = len(d)
     totalDist = 0
     for i, e in enumerate(d):
@@ -40,13 +39,28 @@ def fitness(d = []):
         else:
             dist = node.transit_info_id('public', e, d[0]).get('dist')
         totalDist += dist
-    print('total dist', dist)
-    return dist
+    print('total dist', totalDist)
+    return totalDist
 
 c = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 shuffle(c)
 print(c)
 
-fitness(c)
+bestDist = 9999
+for i in range(0, 10000):
+    # seed(i)
+    dist = fitness(c)
+
+    def randSeed():
+        return .00001
+
+    # shuffle(c, randSeed)
+    # seed(909)
+    shuffle(c)
+    if dist < bestDist:
+        bestDist = dist
+        print('found best new dist', dist)
+print('dist', bestDist)
+    
 
 
