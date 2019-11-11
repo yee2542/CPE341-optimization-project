105aeb84d8a2ea2f1b2094a7f198e87983c7fb73
yee2542 <yee2542gtl@gmail.com>
2019-11-11 21:00:28 +0700

    add permutation.js

---
+++ permutation.js
@@ -0,0 +1,19 @@
+var permArr = [],
+  usedChars = [];
+
+function permute(input) {
+  var i, ch;
+  for (i = 0; i < input.length; i++) {
+    ch = input.splice(i, 1)[0];
+    usedChars.push(ch);
+    if (input.length == 0) {
+      permArr.push(usedChars.slice());
+    }
+    permute(input);
+    input.splice(i, 0, ch);
+    usedChars.pop();
+  }
+  return permArr
+};
+
+console.log(permute([1,2,3,4,5,6,7,8,9,10]))
