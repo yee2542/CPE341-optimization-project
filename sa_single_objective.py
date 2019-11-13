91c93afcd9f0b2c839193e1ac15b9cb86297aeed
yee2542 <yee2542gtl@gmail.com>
2019-11-13 23:47:50 +0700

    add subplot

--- sa_single_objective.py
+++ sa_single_objective.py
@@ -105,18 +105,20 @@ def sa(data):
     print('best solution', historySolutions[-1:][0])
     # print('accept solution', acceptSolution)
 
-    # plt.plot(range(0, len(acceptSolutions)), acceptSolutions, color='green', linewidth = .5, marker='x')
+    plt.subplot(121)
+    plt.plot(range(0, len(acceptSolutions)), acceptSolutions, color='green', linewidth = .5, marker='x')
+
     # plt.legend() 
     # plt.show()
     # print('visualNode', visualNode)
-    # visual(historySolutions[-1:])
+    plt.subplot(122)
     visualPlt = visual(historySolutions[-1:][0])
     visualPlt.show()
     # plt.show()
 
 
 
-# sa([0, 1, 2, 3, 4, 5, 6, 7, 8])
-sa([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+sa([0, 1, 2, 3, 4, 5, 6, 7, 8])
+# sa([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
 
 
