61f77b2326060b3377407fbd8d76d310497127bb
yee2542 <yee2542gtl@gmail.com>
2019-11-14 12:14:09 +0700

    add realtime visualization

--- sa_single_objective.py
+++ sa_single_objective.py
@@ -45,8 +45,8 @@ def sa(data):
     bestDist = 9999999
     history = []
     deltaE_avg = 0.0
-    n = 130                 # step to lower temp
-    m = 50                 # step of each neibor finding solution
+    n = 20                 # step to lower temp
+    m = 25                 # step of each neibor finding solution
     T = 20
     distCandidate = fitness(data)[0]
     # fraction reduction every cycle
@@ -95,6 +95,18 @@ def sa(data):
                 # increment number of accept solution
                 na = na + 1.0
                 deltaE_avg = (deltaE_avg * (na-1.0) +  deltaE) / na
+
+                # plot
+                plt.subplot(121)
+                plt.pause(0.0000000000005)
+                # plt.pause(0.000001)
+                plt.plot(range(0, len(acceptSolutions)), acceptSolutions, color='green', linewidth = .5, marker='x')
+                # plt.plot(range(0, 1000), dist, color='green', linewidth = .5, marker='x')
+                plt.subplot(122)
+                plt.cla()
+                visualPlt = visual(historySolutions[-1:][0])
+                # plt.clf(visualPlt)
+
         T = frac * T
         print('prob', p)
         print('na', na)
@@ -105,20 +117,20 @@ def sa(data):
     print('best solution', historySolutions[-1:][0])
     # print('accept solution', acceptSolution)
 
-    plt.subplot(121)
-    plt.plot(range(0, len(acceptSolutions)), acceptSolutions, color='green', linewidth = .5, marker='x')
+    # plt.subplot(121)
+    # plt.plot(range(0, len(acceptSolutions)), acceptSolutions, color='green', linewidth = .5, marker='x')
 
     # plt.legend() 
     # plt.show()
     # print('visualNode', visualNode)
-    plt.subplot(122)
-    visualPlt = visual(historySolutions[-1:][0])
-    visualPlt.show()
-    # plt.show()
+    # plt.subplot(122)
+    # visualPlt = visual(historySolutions[-1:][0])
+    # visualPlt.show()
+    plt.show()
 
 
 
-sa([0, 1, 2, 3, 4, 5, 6, 7, 8])
-# sa([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
+# sa([0, 1, 2, 3, 4, 5, 6, 7, 8])
+sa([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13])
 
 
